3f8ed4af7de771e8aa4d6728fc2b27275b79c0ac
PixelPlip <philip.wingfelt@gmail.com>
2024-12-02 11:26:47 +0100

    Cleanup

--- dec1/location-id.sh
+++ dec1/location-id.sh
@@ -6,17 +6,15 @@ main() {
   local -a left right
 
   while read -r id1 id2; do
-    left+=("$id1")
-    right+=("$id2")
+    left+=("${id1}")
+    right+=("${id2}")
   done<"$1"
 
   IFS=$'\n' left=($(sort -n <<<"${left[*]}"))
   IFS=$'\n' right=($(sort -n <<<"${right[*]}"))
 
   for ((index = 0; index < "${#left[@]}"; index++)); do
-    ((distance=left[index]-right[index]))
-    ((distance < 0)) && ((distance*=-1))
-    ((total+=distance))
+    (( total += (distance = left[index] - right[index]) < 0 ? -distance : distance ))
   done
 
   echo "${total}"
