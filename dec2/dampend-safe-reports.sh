222042b5f28e288c2e15d075d5cc79e00e501c65
PixelPlip <philip.wingfelt@gmail.com>
2024-12-05 16:27:33 +0100

    Second puzzle of day two

--- dec2/dampend-safe-reports.sh
+++ dec2/dampend-safe-reports.sh
@@ -13,6 +13,26 @@ validate_report() {
   echo 1
 }
 
+dampen_validation() {
+  local -a numbers sub_numbers
+  numbers=($1)
+  for ((i = 0; i < ${#numbers[@]}; i++)); do
+    sub_numbers=$(remove_index_from_array "${numbers[*]}" "$i")
+    if [[ $(validate_report "${sub_numbers[*]}") -eq 1 ]]; then
+      echo 1
+      return 0
+    fi
+  done
+  echo 0
+}
+
+remove_index_from_array() {
+  local -a array
+  array=($1)
+  unset 'array[$2]'
+  echo "${array[*]}"
+}
+
 main() {
   local report
   local -i safe_reports
@@ -20,7 +40,9 @@ main() {
 
   while read -r report; do
     numbers=(${report})
-    (( safe_reports += $(validate_report "${numbers[*]}") ))
+    if [[ $(validate_report "${numbers[*]}") -eq 1 || $(dampen_validation "${numbers[*]}") -eq 1 ]]; then
+     (( safe_reports++ ))
+   fi
   done<"$1"
 
   echo "${safe_reports}"
