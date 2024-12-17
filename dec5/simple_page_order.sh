813dbf08910f2b63bf07f9bbc7928102047ed967
PixelPlip <philip.wingfelt@gmail.com>
2024-12-17 10:49:46 +0100

    Improved readability

--- dec5/simple_page_order.sh
+++ dec5/simple_page_order.sh
@@ -1,19 +1,40 @@
 #!/usr/bin/env bash
 
-# Read rules to assosiative array
-while read -r rule; do
-  PRINT_RULES[${rule//|/}]=1
-done< <(grep -oP "^\d+\|\d+$" <"$1")
+declare -A PRINT_RULES
 
-while IFS="," read -r -a line_pages; do
-  (( valid_line = 1 ))
-  for (( i = 0; i < ${#line_pages[@]} - 1; i++ )); do
-    if [[ -z ${PRINT_RULES[ ${line_pages[i]}${line_pages[$((i+1))]} ]} ]]; then
-      (( valid_line = 0 ))
-      break
-    fi
-  done
-  (( valid_line )) && (( total += line_pages[ (${#line_pages[@]} / 2) ] ))
-done< <(grep -oP "^(\d+,)+\d+$" <"$1")
+read_rules_from_file() {
+  local rule
+  while read -r rule; do
+    PRINT_RULES[${rule//|/}]=1
+  done< <(grep -oP "^\d+\|\d+$" <"$1")
+}
 
-echo "$total"
+check_page_before_page() {
+  if [[ -n ${PRINT_RULES[$1$2]} ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+main() {
+  local -a line_pages
+  local -i valid_line total i number_of_pages
+  read_rules_from_file "$1"
+
+  while IFS="," read -r -a line_pages; do
+    (( valid_line = 1 ))
+    (( number_of_pages = ${#line_pages[@]} ))
+    for (( i = 0; i < number_of_pages - 1; i++ )); do
+      if ! check_page_before_page "${line_pages[i]}" "${line_pages[$((i+1))]}"; then
+        (( valid_line = 0 ))
+        break
+      fi
+    done
+    (( valid_line )) && (( total += line_pages[ (number_of_pages / 2) ] ))
+  done< <(grep -oP "^(\d+,)+\d+$" <"$1")
+
+  echo "$total"
+}
+
+main "$@"
